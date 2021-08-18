b658def26c42d3b40b0075f5319d23c8fc83b2a3
noel zhang <noel.zhang@yuanqu-tech.com>
2021-08-18 16:05:42 +0800

    new dir

---
+++ helloworld/hello.go
@@ -0,0 +1,57 @@
+package helloworld
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+const chinese = "chinese"
+const perchinese = "你好，"
+const World = "World"
+const spanish = "Spanish"
+const spanishHelloPrefix = "Hola, "
+
+// func Hello(name string, language string) string {
+// 	if name == "" {
+// 		name = World
+// 	}
+	// if language == chinese {
+	// 	return perchinese + name
+	// }
+	// if language == spanish {
+    //     return spanishHelloPrefix + name
+    // }
+	// 重构分支语句
+// 	switch language {
+// 	case chinese:
+// 		return perchinese + name
+// 	case spanish:
+// 		return spanishHelloPrefix + name
+// 	}
+
+// 	return englishHelloPrefix + name
+// }
+
+func Hello(name string, language string) string {
+    if name == "" {
+        name = "World"
+    }
+
+    return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+    switch language {
+    case chinese:
+        prefix = perchinese
+    case spanish:
+        prefix = spanishHelloPrefix
+    default:
+        prefix = englishHelloPrefix
+    }
+    return
+}
+
+
+
+func main() {
+	fmt.Println(Hello("charles", ""))
+}
