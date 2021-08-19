6b0a5e509267ff420e5fd33e77fd919f1fcb5b15
noel zhang <noel.zhang@yuanqu-tech.com>
2021-08-19 09:06:16 +0800

    add

--- integers/add_test.go
+++ integers/add_test.go
@@ -1,6 +1,9 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdder(t *testing.T) {
 	sum := Add(2, 2)
@@ -9,4 +12,10 @@ func TestAdder(t *testing.T) {
 	if sum != expect {
 		t.Errorf("expect '%d' but get '%d'", expect, sum)
 	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum) //如果没有Output，那么该段代码虽然会被编译，但是不会被执行
+	// Output: 6
 }
